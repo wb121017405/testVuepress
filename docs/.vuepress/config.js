80c4474b683314434c7b9bfe8aab2a351fa80fab
wb121017405 <121017405@qq.com>
2020-06-02 14:42:38 +0800

    就是这样

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,37 @@
 module.exports = {
   title: "标题",
   base: "/testVuepress/",
-  description: "描述"
+  description: "描述",
+  // 引入百度统计
+  head: [
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    [
+      "script",
+      {},
+      `var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?d8a6fdbfb8332bed5afb8abbed2f572c";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+      })();`
+    ]
+  ],
+  plugins: [
+    [
+      // 评论插件
+      "vuepress-plugin-comment",
+      {
+        choosen: "valine",
+        options: {
+          el: "#valine-vuepress-comment",
+          appId: 'Your own appId',
+          appKey: 'Your own appKey',
+          meta:['nick','mail'],
+          path:'<%- frontmatter.to.path %>',
+          placeholder: "来喷我呀笨蛋！"
+        }
+      }
+    ]
+  ]
 };
